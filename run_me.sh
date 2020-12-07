8ad8d17cd60de18e34325bfbe34eb551be687905
Pawel Jozef Podhajski <pawel.podhajski@epfl.ch>
2020-12-07 11:40:30 +0100

    wip

--- run_me.sh
+++ run_me.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+BRION_TAG=3.3.0
+
 git clone https://github.com/Bluebrain/Brion.git
 pushd Brion
 git submodule update --init --recursive
+git checkout "${BRION_TAG}"
 #TODO: merge it to Brion
 git apply ../cmake.patch
 popd
